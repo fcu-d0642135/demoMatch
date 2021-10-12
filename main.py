43d74612a1e356865080e3c8add68b667838549e
fcu-d0642135 <sh43204327@gmail.com>
2021-10-12 23:19:12 +0800

    feat: 新增 供需雙方的功能選項

--- main.py
+++ main.py
@@ -15,10 +15,54 @@ def identify(username):
 
 
 def supplyChoice():
-    pass
+    choices = {
+        '0': {
+            'name': '設定媒合需求',
+            'func': setMatchRequirements,
+        },
+        '1': {
+            'name': '查詢媒合結果',
+            'func': selectMatchResult,
+        }
+    }
+    
+    for i in choices:
+        print(f'{i}: {choices[i]["name"]}')
+    choice = input('請選擇 ...')
+    choices[choice]['func']()
 
 
 def demandChoice():
+    choices = {
+        '0': {
+            'name': '設定求職需求',
+            'func': setJobHunting,
+        },
+        '1': {
+            'name': '查詢求職結果',
+            'func': selectJobHunting,
+        }
+    }
+
+    for i in choices:
+        print(f'{i}: {choices[i]["name"]}')
+    choice = input('請選擇 ...')
+    choices[choice]['func']()
+
+
+def setMatchRequirements():
+    pass
+
+
+def selectMatchResult():
+    pass
+
+
+def setJobHunting():
+    pass
+
+
+def selectJobHunting():
     pass
 
 
